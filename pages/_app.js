60f9b7d4d28cb303259c44242209066cc7fa1820
Luan Ehm <89112711+luanehm@users.noreply.github.com>
2025-09-01 17:11:43 +0000

    style: aplica formatação inicial com Prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import '../assets/fontawesome/css/font-awesome.min.css';
-import '../assets/fonts/sf-pro/font.css';
-import '../assets/css/reset.css';
-import '../assets/css/style.css';
-import '../assets/css/header.css';
-import '../assets/css/footer.css';
-import '../assets/css/pages/home.css';
-import '../assets/css/pages/categoria.css';
-import '../assets/css/pages/contato.css';
+import "../assets/fontawesome/css/font-awesome.min.css";
+import "../assets/fonts/sf-pro/font.css";
+import "../assets/css/reset.css";
+import "../assets/css/style.css";
+import "../assets/css/header.css";
+import "../assets/css/footer.css";
+import "../assets/css/pages/home.css";
+import "../assets/css/pages/categoria.css";
+import "../assets/css/pages/contato.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
